bb421f0c3cbbc5e7591867e6e3abb40f48cfae27
Michael Dickensheets <dickensheets@s2corporation.com>
2019-04-02 14:48:22 -0700

    Added Tmux Setup Script

---
+++ tmux_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Tmux Package Manager
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+### After running the script, launch tmux and hit "prefix + I". ###
